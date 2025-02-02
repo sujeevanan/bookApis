bb7ecdf08c75cb95a73b5b1bac72e317bafac155
Sujeevanan Loganathan <lsujee2000@gmail.com>
2025-02-02 09:33:32 +0530

    server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
 //server corks on the port in env or in the given port
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
